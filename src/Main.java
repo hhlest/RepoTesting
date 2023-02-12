eda58bae13fc5dd00aa88815b2f712291b4adef8
Waminms <67516571+Waminms@users.noreply.github.com>
2023-02-12 21:54:09 +0200

    TestingMain --> Testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TestingMain");
+        System.out.println("Testing");
+        System.out.println("Testing");
+        System.out.println("Testing");
     }
 }
